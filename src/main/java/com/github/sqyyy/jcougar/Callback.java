7f6cdf357bb96417a7a954209c47e957f803fdf9
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-27 16:03:19 +0100

    Add empty callbacks

--- src/main/java/com/github/sqyyy/jcougar/Callback.java
+++ src/main/java/com/github/sqyyy/jcougar/Callback.java
@@ -8,18 +8,30 @@ import org.jetbrains.annotations.NotNull;
 
 public interface Callback {
     interface Click extends Callback {
+        Click EMPTY = (player, view, slot) -> {
+        };
+
         void click(@NotNull Player player, @NotNull InventoryView view, int slot);
     }
 
     interface Update extends Callback {
+        Update EMPTY = (player, view) -> {
+        };
+
         void update(@NotNull Player player, @NotNull InventoryView view);
     }
 
     interface Open extends Callback {
+        Open EMPTY = (player, inventory) -> {
+        };
+
         void open(@NotNull Player player, @NotNull Inventory inventory);
     }
 
     interface Close extends Callback {
+        Close EMPTY = (player, view, reason) -> {
+        };
+
         void close(@NotNull Player player, @NotNull InventoryView view, InventoryCloseEvent.@NotNull Reason reason);
     }
 }
