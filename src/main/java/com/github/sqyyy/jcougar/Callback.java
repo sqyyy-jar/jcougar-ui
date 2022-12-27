2fa5186ddab522052fff3804ddb0b2423f613204
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-27 15:52:34 +0100

    Add Callback interface

---
+++ src/main/java/com/github/sqyyy/jcougar/Callback.java
@@ -0,0 +1,21 @@
+package com.github.sqyyy.jcougar;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryView;
+import org.jetbrains.annotations.NotNull;
+
+public interface Callback {
+    interface Click extends Callback {
+        void click(@NotNull Player player, @NotNull InventoryView view, int slot);
+    }
+
+    interface Open extends Callback {
+        void open(@NotNull Player player, @NotNull Inventory inventory);
+    }
+
+    interface Close extends Callback {
+        void close(@NotNull Player player, @NotNull InventoryView view, InventoryCloseEvent.@NotNull Reason reason);
+    }
+}
