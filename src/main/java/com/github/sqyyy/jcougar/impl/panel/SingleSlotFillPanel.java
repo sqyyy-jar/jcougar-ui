2913ede742e088d6a2832b72f5da8c354ceb4a2a
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-28 16:18:28 +0100

    Implement AbstractSlotPanel for SingleSlotFillPanel

--- src/main/java/com/github/sqyyy/jcougar/impl/panel/SingleSlotFillPanel.java
+++ src/main/java/com/github/sqyyy/jcougar/impl/panel/SingleSlotFillPanel.java
@@ -1,24 +1,17 @@
 package com.github.sqyyy.jcougar.impl.panel;
 
-import com.github.sqyyy.jcougar.Panel;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class SingleSlotFillPanel implements Panel {
-    private final int slot;
+public class SingleSlotFillPanel extends AbstractSlotPanel {
     private final ItemStack fillItem;
 
     public SingleSlotFillPanel(int slot, @Nullable ItemStack fillItem) {
-        this.slot = slot;
+        super(slot);
         this.fillItem = fillItem;
-    }
-
-    @Override
-    public boolean collidesWith(int slot) {
-        return slot == this.slot;
     }
 
     @Override
