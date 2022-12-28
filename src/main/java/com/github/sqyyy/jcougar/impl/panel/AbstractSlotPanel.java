872bb3f7f8665085e3aaf2bff42ce3f680642e0c
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-28 16:17:36 +0100

    Make AbstractSlotPanel#slot protected

--- src/main/java/com/github/sqyyy/jcougar/impl/panel/AbstractSlotPanel.java
+++ src/main/java/com/github/sqyyy/jcougar/impl/panel/AbstractSlotPanel.java
@@ -3,7 +3,7 @@ package com.github.sqyyy.jcougar.impl.panel;
 import com.github.sqyyy.jcougar.Panel;
 
 public abstract class AbstractSlotPanel implements Panel {
-    private final int slot;
+    protected final int slot;
 
     protected AbstractSlotPanel(int slot) {
         this.slot = slot;
